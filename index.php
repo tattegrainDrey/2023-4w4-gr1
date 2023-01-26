46297725bad8dd4cb0cc49337227eeac0084c4c0
D. A. A. Tattegrain <djenahaat@gmail.com>
2023-01-26 11:51:34 -0500

    s1c2 exemple substituant de texte

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 
 <main>
     <h1>Bienvenue au 4w4</h1>  
+    <section>
+        <article>Voici un exemple de texte qui pourrait se tenir au milieu de mon thème</article>
+        <article> Les paragraphes pourraient être en colonnes pour donner un air journalistique</article>
+        <article>Porttitor enim, quisque elit elementum habitant. Massa justo mattis risus, pellentesque pulvinar class porttitor curae. In erat cursus ullamcorper, eget odio convallis rhoncus ultrices. Leo nisl, sollicitudin neque ante. Curabitur orci molestie, donec nec, euismod hendrerit aliquet ad massa. Volutpat vehicula consectetur conubia, vitae etiam ultrices nisi nullam. Fermentum cursus ultrices, accumsan morbi, gravida himenaeos molestie nunc semper sodales. <br />Nostra nisl egestas, lacus inceptos, magna ante porttitor amet sit libero dui. Proin justo, id accumsan fames dictum. Dui felis lacinia, imperdiet nunc, faucibus suscipit interdum vel ultrices rutrum. Aptent mi est, turpis sit, donec justo erat morbi nam ipsum mollis. Tortor ultrices neque enim, urna nunc nullam tincidunt aliquam.</article>
+    </section>
 </main>
 
 <?php get_footer(); ?> 
