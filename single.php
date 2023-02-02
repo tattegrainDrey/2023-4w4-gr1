8c46e215364946b055b7886af380d0c0ce2f02e6
D. A. A. Tattegrain <djenahaat@gmail.com>
2023-02-02 11:49:15 -0500

    s2c2 ajout de front-page.php et single.php

--- single.php
+++ single.php
@@ -1,16 +1,16 @@
 <?php 
-/** index.php est le modele par défaut du thème
+/** single.php est le modele par défaut du thème
 */ ?> 
 <?php get_header(); ?> 
 
 <main>
-    <pre>front-page.php</pre>
+    <pre>single.php</pre>
     <h1>Bienvenue au 4w4</h1>  
     <?php if(have_posts()):
         while(have_posts()): the_post(); ?> 
             <article>
-                <a href="<?php the_permalink() ?>"><h3><?php the_title() ?> </h3></a>
-                <h4><?php the_excerpt() ?> </h4>
+                <h3><?php the_title() ?> </h3>
+                <h4><?php the_content() ?> </h4>
 
             </article>
             <hr>
