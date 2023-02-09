b8a8153a04506b2f36d7cd01134e682f83430a5e
D. A. A. Tattegrain <djenahaat@gmail.com>
2023-02-09 12:24:24 -0500

    s3c2 déplacement d'éléments dans l'entete et brefs changement de style

--- header.php
+++ header.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-    <h2><?= bloginfo('description') ?> </h2>
     <?php wp_head(); ?>
 </head>
 <body>
     <header class="site__entete">
-        <div class="entete__gauche">
-            <?php the_custom_logo(); ?>
-            <a class="site__titre"href="<?= bloginfo('url') ?>"><h1><?= bloginfo('name') ?> </h1></a> 
-        </div>
-        <?php  wp_nav_menu(array( 
+            <div class="entete__gauche">
+                <div class="image">
+                    <?php the_custom_logo(); ?>
+                </div>
+                <div class="titre__slogan">  
+                    <a class="site__titre"href="<?= bloginfo('url') ?>"><h1><?= bloginfo('name') ?> </h1></a>
+                    <h2><?= bloginfo('description') ?> </h2> 
+                </div>
+            </div>
+            <?php  wp_nav_menu(array( 
                     "menu" => "entete",
                     "container" => "nav",
                     "container_class" => "menu__entete"
-                 )); ?>
+             )); ?>
     </header>
