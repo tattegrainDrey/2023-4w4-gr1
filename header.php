74080f6081b1f517428337a72e8552c50a9ede29
D. A. A. Tattegrain <djenahaat@gmail.com>
2023-02-09 11:50:33 -0500

    s3c2 modification de style sur l'entete

--- header.php
+++ header.php
@@ -3,15 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <a class="site__titre"href="<?= bloginfo('url') ?>"><h1><?= bloginfo('name') ?> </h1></a>    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
     <h2><?= bloginfo('description') ?> </h2>
     <?php wp_head(); ?>
 </head>
 <body>
     <header class="site__entete">
-       <?php the_custom_logo(); ?> 
-       <?php  wp_nav_menu(array( 
+        <div class="entete__gauche">
+            <?php the_custom_logo(); ?>
+            <a class="site__titre"href="<?= bloginfo('url') ?>"><h1><?= bloginfo('name') ?> </h1></a> 
+        </div>
+        <?php  wp_nav_menu(array( 
                     "menu" => "entete",
                     "container" => "nav",
                     "container_class" => "menu__entete"
