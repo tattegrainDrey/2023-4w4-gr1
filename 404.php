909982fadefdd7441b64cbf02567868494a7ae2e
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2023-03-30 10:11:54 -0400

    intra: début de css. centrer le flexbox et ajout de la barre de recherche

--- 404.php
+++ 404.php
@@ -6,20 +6,21 @@
 
 <main class="e404">
     <!-- <pre>404.php</pre> -->
-    <h1>Erreur 404</h1>
-    <div class="search404">
-        <p>Page introuvable, vous pouvez tenter une recherche</p>
-        <p>Placeholder recherche</p>
-    </div>
-    <div class="coursmenu404">
-        <p>Nos choix de cours</p>
-        <div>Items Cours Menu Placeholder</div>
-    </div>
-    <div class="notesmenu404">
-        <p>Les notes de cours</p>
-        <div>Items Notes Menu Placeholder</div>
-    </div>
-
+        <h1>Erreur 404</h1>
+        <div class="search404">
+            <h2>Page introuvable, vous pouvez tenter une recherche</h2>
+            <?php
+                get_search_form();
+             ?>
+        </div>
+        <div class="coursmenu404">
+            <p>Nos choix de cours</p>
+            <div>Items Cours Menu Placeholder</div>
+        </div>
+        <div class="notesmenu404">
+            <p>Les notes de cours</p>
+            <div>Items Notes Menu Placeholder</div>
+        </div>
 </main>
 </div>
 <?php get_footer(); ?>
