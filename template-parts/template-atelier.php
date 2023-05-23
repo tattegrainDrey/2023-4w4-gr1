9e6e6308a4eea9c54cdcef233cc960ae882f9f62
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2023-05-23 10:22:46 -0400

    ef-dat : debut de modele atelier

---
+++ template-parts/template-atelier.php
@@ -0,0 +1,21 @@
+<?php
+    /**
+     * Template name: Atelier
+     */
+?>
+<?php get_header(); ?>
+<main class="page">
+<?php
+if ( have_posts() ) : the_post(); ?>
+<h1><?= get_the_title(); ?></h1>
+<?php the_content();?>
+<p>Formateur: <?php the_field('formateur'); ?></p>
+<p>Date de l'atelier: <?php the_field('date'); ?></p>
+<p>Heure de l'atelier: <?php the_field('heure'); ?></p>  
+<p>Durée d'un atelier: <?php the_field('duree'); ?> heures</p>
+<p>Local de l'atelier: <?php the_field('local'); ?></p>  
+<?php endif;?>
+</main><!-- #main -->
+</div>
+<?php
+get_footer();
